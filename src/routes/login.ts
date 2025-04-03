329864dd7362086c193f42fd9828abd5b4a78a4f
AsiAtias <asia@cymulate.com>
2025-04-03 19:57:23 +0300

    replace bcrypt with bcryptjs

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -1,5 +1,5 @@
 import express, {Request, Response, Router} from 'express';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import User, {IUser} from '../models/User.js';
 
